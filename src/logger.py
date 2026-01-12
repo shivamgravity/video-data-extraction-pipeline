1af72750e9586af7f1cb906c49ef9335716106d9
Shivam Gravity <shivamgravity@gmail.com>
2026-01-12 23:15:50 +0530

    updated the logger logic

---
+++ src/logger.py
@@ -0,0 +1,16 @@
+import logging
+from pathlib import Path
+
+def setup_logger():
+    Path("logs").mkdir(exist_ok=True)
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname)s | %(message)s",
+        handlers=[
+            logging.FileHandler("logs/app.log"),
+            logging.StreamHandler()
+        ]
+    )
+
+    return logging.getLogger("yt-frame-extractor")
