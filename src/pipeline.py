24d2d896a721c77e3d3124afaca287464049f1e6
Shivam Gravity <shivamgravity@gmail.com>
2026-01-12 23:13:37 +0530

    updated the pipeline

---
+++ src/pipeline.py
@@ -0,0 +1,10 @@
+import shutil
+from downloader import download_video
+from ffmpeg_utils import extract_frames_ffmpeg
+
+def process_video(url, output_dir, interval, logger):
+    video_path, temp_dir = download_video(url, logger)
+    count = extract_frames_ffmpeg(video_path, output_dir, interval, logger)
+    shutil.rmtree(temp_dir)
+    logger.info("Temporary files cleaned up")
+    return count
